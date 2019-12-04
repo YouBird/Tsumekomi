ca7543bbeccf930819a30f570fd69383e2d8b064
YouBird <c129038r@comwork.c.oka-pu.ac.jp>
2019-12-04 16:34:44 +0900

    [初回]

---
+++ first_app/config/routes.rb
@@ -0,0 +1,15 @@
+Rails.application.routes.draw do
+#  get 'boards' => 'boards#index'
+#  get 'boards/new' => 'boards#new'
+#  post 'boards' => 'boards#create'
+#  get 'boards/:id' => 'boards#show'
+#  get 'boards/:id/edit' => 'boards#edit'
+#  put 'boards/:id' => 'boards#update'
+#  delete 'boards/:id' => 'boards#destroy'
+
+resources :boards do
+  resources :comments, only: [:create, :destroy]
+end
+  # The priority is based upon order of creation: first created -> highest priority.
+  # See how all your routes lay out with "rake routes".
+end
